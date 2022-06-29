2a6f1dd9c396fdb0ea639351494f8faf4c4f687f
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2022-06-29 23:37:40 +0300

    Add files via upload

---
+++ 02. Center Point/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._Center_Point
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] result = new double[2];
+            double sum = double.MaxValue;
+
+            for (int i = 0; i < 2; i++)
+            {
+                double x = double.Parse(Console.ReadLine());
+                double y = double.Parse(Console.ReadLine());
+
+                GetTheClosetPoint(x, y, result, ref sum);
+            }
+            Console.WriteLine($"({string.Join(", ", result)})");
+        }
+
+
+        static void GetTheClosetPoint(double x, double y, double[] result, ref double sum) 
+        {
+            double curSum = Math.Abs(Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2)));
+
+            if (curSum < sum)
+            {
+                sum = curSum;
+
+                result[0] = x;
+                result[1] = y;
+            }
+
+        }
+    }
+}
