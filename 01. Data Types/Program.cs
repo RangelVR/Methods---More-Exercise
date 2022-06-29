e4dbba00306c1e98cda2cd716a0404d2d347df6b
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2022-06-29 22:20:43 +0300

    Add files via upload

---
+++ 01. Data Types/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _01._Data_Types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dataType = Console.ReadLine();
+            string data = Console.ReadLine();
+
+            if (dataType == "int")
+            {
+                PrintDataInt(dataType, data);
+            }
+            else if (dataType == "real")
+            {
+                PrintDataReal(dataType, data);
+            }
+            else if (dataType == "string")
+            {
+                Console.WriteLine($"${data}$");
+            }
+        }
+
+
+        static void PrintDataInt(string dataType, string data) 
+        {
+            int number = int.Parse(data);
+            Console.WriteLine(number * 2);
+        }
+
+
+        static void PrintDataReal(string dataType, string data) 
+        {
+            double number = double.Parse(data);
+            double sum = number * 1.5;
+            Console.WriteLine($"{sum:f2}");
+        }
+
+    }
+}
