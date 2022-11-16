fc13f4ce0acefcda1b7847db3aea91511506a3db
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2022-11-16 23:59:31 +0200

    Update Program.cs

--- 05. Multiplication Sign/Program.cs
+++ 05. Multiplication Sign/Program.cs
@@ -1,6 +1,6 @@
-﻿using System;
+using System;
 
-namespace _05._Multiplication_Sign
+namespace _04.Tribonacci_Sequence
 {
     class Program
     {
@@ -10,33 +10,31 @@ namespace _05._Multiplication_Sign
             int n2 = int.Parse(Console.ReadLine());
             int n3 = int.Parse(Console.ReadLine());
 
-            GetResultOfTheNumbers(n1, n2, n3);
+            PrintPositiveOrNegative(n1, n2, n3);
         }
 
 
-        static void GetResultOfTheNumbers(int n1, int n2, int n3)
+        static void PrintPositiveOrNegative(int n1, int n2, int n3)
         {
             if (n1 == 0 || n2 == 0 || n3 == 0)
             {
                 Console.WriteLine("zero");
             }
-            else if (n1 > 0 && n2 > 0 && n3 > 0)
-            {
-                Console.WriteLine("positive");
-            }
             else if (n1 < 0 && n2 < 0 && n3 < 0)
             {
                 Console.WriteLine("negative");
             }
-            else if (n1 < 0 && n2 < 0
-                  || n1 < 0 && n3 < 0
-                  || n2 < 0 && n3 < 0)
+            else if (n1 < 0 && n2 < 0 || n1 < 0 && n3 < 0 || n2 < 0 && n3 < 0)
             {
                 Console.WriteLine("positive");
             }
-            else if (n1 < 0 || n2 < 0 || n3 < 0)
+            else if(n1 < 0 || n2 < 0 || n3 < 0)
             {
                 Console.WriteLine("negative");
+            }
+            else
+            {
+                Console.WriteLine("positive");
             }
         }
     }
