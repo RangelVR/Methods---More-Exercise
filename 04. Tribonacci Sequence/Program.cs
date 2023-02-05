0a2d2c03affd56ff37d98df8b9a41da30716ca6f
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2023-02-06 00:10:10 +0200

    Update Program.cs

--- 04. Tribonacci Sequence/Program.cs
+++ 04. Tribonacci Sequence/Program.cs
@@ -1,24 +1,19 @@
-using System;
 using System.Linq;
 
 namespace _04._Tribonacci_Sequence
 {
-    class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
             int n = int.Parse(Console.ReadLine());
-            int[] arr = new int[Math.Max(n, 3)];
+            int[] tribonacciSequence = new int[Math.Max(n, 3)];
 
-            GetTribonacciSequence(n, arr);
-
-            for (int i = 0; i < n; i++)
-            {
-                Console.Write($"{arr[i]} ");
-            }
+            GetTribonacciSequence(ref tribonacciSequence, n);
+            PrintTribonacciSequence(tribonacciSequence, n);
         }
 
-        static void GetTribonacciSequence(int n, int[] arr)
+        static void GetTribonacciSequence(ref int[] arr, int n)
         {
             arr[0] = arr[1] = 1;
             arr[2] = 2;
@@ -27,8 +22,20 @@ namespace _04._Tribonacci_Sequence
             {
                 arr[i] = arr[i - 1] + arr[i - 2] + arr[i - 3];
             }
+            //for (int i = 0; i < n - 3; i++)
+            //{
+            //    int[] arr = tribonacciSequence.Skip(i).Take(3).ToArray();
+            //    tribonacciSequence[i + 3] = arr.Sum();
+            //}
         }
 
+        static void PrintTribonacciSequence(int[] arr, int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("{0} ", arr[i]);
+            }
+        }
     }
 }
 ==============================================================================================================
